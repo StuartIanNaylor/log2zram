f45394c7f6666b824c450e244612fb4849ba8206
StuartIanNaylor <stuartiannaylor@outlook.com>
2019-03-16 12:58:53 +0000

    More cleaning add mkdir /var/log/oldlog

--- install.sh
+++ install.sh
@@ -24,6 +24,7 @@ mkdir -p /var/hdd.log
 mkdir -p /var/log/oldlog
 chmod 754 /var/log/oldlog
 chown root:adm /var/log/oldlog
+
 # Prune logs
 cp -au /var/log/*.1 /var/log/oldlog > /dev/null 2>&1 &
 cp -au /var/log/*.gz /var/log/oldlog > /dev/null 2>&1 &
@@ -36,7 +37,7 @@ rm -r /var/log/*.old > /dev/null 2>&1 &
 echo "#####               Clone /var/log                 #####"
 rsync -arzhv /var/log/ /var/hdd.log/
 
-sed -i '/^include.*/i olddir /var/log/oldlog' /etc/logrotate.conf
+sed -i '/^weekly.*/i olddir /var/log/oldlog' /etc/logrotate.conf
 
 echo "#####          Reboot to activate log2zram         #####"
 echo "##### edit /etc/log2zram.conf to configure options #####"
