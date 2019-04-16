e6c092925aa50c74ccd3d6813d397f0b29473394
StuartIanNaylor <stuartiannaylor@outlook.com>
2019-04-16 07:55:27 +0100

    add merge

--- install.sh
+++ install.sh
@@ -4,12 +4,20 @@ systemctl -q is-active log2zram  && { echo "ERROR: log2zram service is still run
 [ "$(id -u)" -eq 0 ] || { echo "You need to be ROOT (sudo can be used)"; exit 1; }
 [ -d /usr/local/bin/log2zram ] && { echo "Log2Zram is already installed, uninstall first"; exit 1; }
 
+apt-get install libattr1-dev
+git clone https://github.com/kmxz/overlayfs-tools
+cd overlayfs-tools
+make
+cd ..
+
 mkdir -p /usr/local/share/log2zram/
 # log2zram install 
 install -m 644 log2zram.service /etc/systemd/system/log2zram.service
 install -m 755 log2zram /usr/local/bin/log2zram
 install -m 644 log2zram.conf /etc/log2zram.conf
 install -m 644 uninstall.sh /usr/local/share/log2zram/uninstall.sh
+mkdir -p /usr/local/lib/log2zram/
+install -m 755 overlayfs-tools/overlay /usr/local/lib/log2zram/overlay
 mkdir -p /usr/local/share/log2zram/log
 systemctl enable log2zram
 
--- uninstall.sh
+++ uninstall.sh
@@ -10,6 +10,7 @@ then
 
 	echo "Log2Zram is uninstalled, removing the uninstaller in progress"
 	rm -rf /usr/local/share/log2zram
+	rm -rf /usr/local/lib/log2zram
 	echo "##### Reboot isn't needed #####"
 else
 	echo "You need to be ROOT (sudo can be used)"
