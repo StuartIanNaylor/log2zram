ae069a1c32932ab192e1606c1aa0c4da46473d40
StuartIanNaylor <stuartiannaylor@outlook.com>
2019-04-16 06:21:55 +0100

    convert to overlayfs

--- install.sh
+++ install.sh
@@ -4,35 +4,18 @@ systemctl -q is-active log2zram  && { echo "ERROR: log2zram service is still run
 [ "$(id -u)" -eq 0 ] || { echo "You need to be ROOT (sudo can be used)"; exit 1; }
 [ -d /usr/local/bin/log2zram ] && { echo "Log2Zram is already installed, uninstall first"; exit 1; }
 
-
+mkdir -p /usr/local/share/log2zram/
 # log2zram install 
-mkdir -p /usr/local/bin/log2zram
 install -m 644 log2zram.service /etc/systemd/system/log2zram.service
-install -m 755 log2zram /usr/local/bin/log2zram/log2zram
+install -m 755 log2zram /usr/local/bin/log2zram
 install -m 644 log2zram.conf /etc/log2zram.conf
-install -m 755 log2zram-schd /usr/local/bin/log2zram/log2zram-schd
-install -m 644 log2zram-schd-pid /usr/local/bin/log2zram/log2zram-schd-pid
-install -m 644 uninstall.sh /usr/local/bin/log2zram/uninstall.sh
+install -m 644 uninstall.sh /usr/local/share/log2zram/uninstall.sh
 systemctl enable log2zram
 
 
 # Make sure we start clean
 rm -rf /var/hdd.log
-mkdir -p /var/log/oldlog
 mkdir -p /var/hdd.log
-mkdir -p /var/prune.log
-
-# Prune logs
-cp -a /var/log/*.1 /var/prune.log > /dev/null 2>&1 &
-cp -a /var/log/*.gz /var/prune.log > /dev/null 2>&1 &
-cp -a /var/log/*.old /var/prune.log > /dev/null 2>&1 &
-
-rm -r /var/log/*.1 > /dev/null 2>&1 &
-rm -r /var/log/*.gz > /dev/null 2>&1 &
-rm -r /var/log/*.old > /dev/null 2>&1 &
+mkdir -p /usr/local/share/log2zram/log
 
 
-sed -i '/^weekly.*/i olddir /var/log/oldlog' /etc/logrotate.conf
-
-echo "#####          Reboot to activate log2zram         #####"
-echo "##### edit /etc/log2zram.conf to configure options #####"
--- uninstall.sh
+++ uninstall.sh
@@ -5,15 +5,11 @@ then
 	service log2zram stop
 	systemctl disable log2zram
 	rm /etc/systemd/system/log2zram.service
-	rm /usr/local/bin/log2zram/log2zram
-	rm /usr/local/bin/log2zram/log2zram-schd
-	rm /usr/local/bin/log2zram/log2zram-schd-pid
+	rm /usr/local/bin/log2zram
 	rm /etc/log2zram.conf
 
-	sudo sed -i '/olddir.*/d' /etc/logrotate.conf
-	mv /usr/local/bin/log2zram/logrotate /etc/cron.daily/logrotate > /dev/null 2>&1 &
 	echo "Log2Zram is uninstalled, removing the uninstaller in progress"
-	rm -rf /usr/local/bin/log2zram
+	rm -rf /usr/local/share/log2zram
 	echo "##### Reboot isn't needed #####"
 else
 	echo "You need to be ROOT (sudo can be used)"
