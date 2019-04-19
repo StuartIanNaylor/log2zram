28b0e84e5001c96c6e0dbdb036d0a5417062141a
S. Bruce <28817345+foundObjects@users.noreply.github.com>
2019-04-19 10:24:35 -0700

    Added a workaround for kernels < 4.10 to solve an error caused by a missing feature. Some small changes and linting. Also added a note in README.md re: compilation issues with overlayfs-tools when kernel and gcc headers are out of sync (damn you rockchip 4.4 kernel.)
    Note: "set -e" won't trigger during a logical shortcut (foo && bar; foo || bar) unless the last operation in the chain returns a `false` value. Nor will
    it trigger during a test block (eg: [ test foo ]).

--- install.sh
+++ install.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+# set -ex
+
 systemctl -q is-active log2zram  && { echo "ERROR: log2zram service is still running. Please run \"sudo service log2zram stop\" to stop it and uninstall"; exit 1; }
 [ "$(id -u)" -eq 0 ] || { echo "You need to be ROOT (sudo can be used)"; exit 1; }
 [ -d /usr/local/bin/log2zram ] && { echo "Log2Zram is already installed, uninstall first"; exit 1; }
 
-apt-get install libattr1-dev
-git clone https://github.com/kmxz/overlayfs-tools
+# save time (if we can) on slow machines/storage loading and parsing apt package lists
+( dpkg -l | grep -q "libattr1-dev" ) || \
+	apt-get install libattr1-dev -y
+[ -d overlayfs-tools ] || \
+	git clone https://github.com/kmxz/overlayfs-tools
 cd overlayfs-tools
 make
 cd ..
@@ -21,7 +26,4 @@ mkdir -p /usr/local/lib/log2zram/
 install -m 755 overlayfs-tools/overlay /usr/local/lib/log2zram/overlay
 mkdir -p /usr/local/share/log2zram/log
 systemctl enable log2zram
-
-
-
 
